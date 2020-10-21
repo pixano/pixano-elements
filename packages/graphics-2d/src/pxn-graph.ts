5a43ea04ce4520ae797f03eaac6e93908b9abe9f
Camille Dupont <camille.dupont@cea.fr>
2020-10-21 18:18:50 +0200

    Release V0.4.2

--- packages/graphics-2d/src/pxn-graph.ts
+++ packages/graphics-2d/src/pxn-graph.ts
@@ -15,71 +15,6 @@ import { settings } from './graph-shape';
 
 export { settings };
 
-/**
- * Inherit Canvas2d to handle graph shapes (keypoints).
- */
-@customElement('pxn-graph' as any)
-export class Graph extends Canvas2d {
-
-    private selectedNodeIdx: number = -1;
-
-    constructor() {
-        super();
-        this.setController('create', new GraphCreateController(this.renderer, this.shapes))
-            .setController('edit', new GraphsUpdateController(this.renderer, this.graphics,
-                                                                          this.targetShapes, this.dispatchEvent.bind(this)));
-    }
-
-    protected firstUpdated() {
-        super.firstUpdated();
-        window.addEventListener('keyup', (evt: KeyboardEvent) => {
-            switch (evt.key) {
-                case 'z': case 's':
-                case 'd': case 'q': {
-                    if (this.selectedNodeIdx !== -1 && this.targetShapes.size === 1 && !evt.ctrlKey) {
-                        const obj = [...this.targetShapes][0];
-                        if (obj) {
-                            this.dispatchEvent(new CustomEvent('update', { detail: [obj.id] }));
-                        }
-                    }
-                    break;
-                }
-            }
-        })
-        window.addEventListener('keydown', (evt: KeyboardEvent) => {
-            switch (evt.key) {
-                case 'z': case 's':
-                case 'd': case 'q': {
-                    if (this.selectedNodeIdx !== -1 && this.targetShapes.size === 1 && !evt.ctrlKey) {
-                        const obj = [...this.targetShapes][0];
-                        if (obj) {
-                            // translate selected node
-                            const xsign = evt.key === 'q' ? -1 : evt.key === 'd' ? 1 : 0;
-                            const ysign = evt.key === 'z' ? -1 : evt.key === 's' ? 1 : 0;
-                            obj.geometry.vertices[2 * this.selectedNodeIdx] += 0.005 * xsign;
-                            obj.geometry.vertices[2 * this.selectedNodeIdx + 1] += 0.005 * ysign;
-                        }
-                    }
-                    break;
-                }
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9': {
-                    this.selectedNodeIdx = Number(evt.key);
-                    break;
-                }
-            }
-        })
-    }
-}
-
 class GraphsUpdateController extends ShapesEditController {
 
     protected activeNodeIdx: number = -1;
@@ -233,5 +168,70 @@ class GraphCreateController extends ShapeCreateController {
         this.renderer.stage.removeChild(shape);
         shape.destroy();
         this.tmpShape = null;
+    }
+}
+
+/**
+ * Inherit Canvas2d to handle graph shapes (keypoints).
+ */
+@customElement('pxn-graph' as any)
+export class Graph extends Canvas2d {
+
+    private selectedNodeIdx: number = -1;
+
+    constructor() {
+        super();
+        this.setController('create', new GraphCreateController(this.renderer, this.shapes))
+            .setController('edit', new GraphsUpdateController(this.renderer, this.graphics,
+                                                                          this.targetShapes, this.dispatchEvent.bind(this)));
+    }
+
+    protected firstUpdated() {
+        super.firstUpdated();
+        window.addEventListener('keyup', (evt: KeyboardEvent) => {
+            switch (evt.key) {
+                case 'z': case 's':
+                case 'd': case 'q': {
+                    if (this.selectedNodeIdx !== -1 && this.targetShapes.size === 1 && !evt.ctrlKey) {
+                        const obj = [...this.targetShapes][0];
+                        if (obj) {
+                            this.dispatchEvent(new CustomEvent('update', { detail: [obj.id] }));
+                        }
+                    }
+                    break;
+                }
+            }
+        })
+        window.addEventListener('keydown', (evt: KeyboardEvent) => {
+            switch (evt.key) {
+                case 'z': case 's':
+                case 'd': case 'q': {
+                    if (this.selectedNodeIdx !== -1 && this.targetShapes.size === 1 && !evt.ctrlKey) {
+                        const obj = [...this.targetShapes][0];
+                        if (obj) {
+                            // translate selected node
+                            const xsign = evt.key === 'q' ? -1 : evt.key === 'd' ? 1 : 0;
+                            const ysign = evt.key === 'z' ? -1 : evt.key === 's' ? 1 : 0;
+                            obj.geometry.vertices[2 * this.selectedNodeIdx] += 0.005 * xsign;
+                            obj.geometry.vertices[2 * this.selectedNodeIdx + 1] += 0.005 * ysign;
+                        }
+                    }
+                    break;
+                }
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9': {
+                    this.selectedNodeIdx = Number(evt.key);
+                    break;
+                }
+            }
+        })
     }
 }
