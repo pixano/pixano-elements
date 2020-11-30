a9aa292474aef6ba1e459b39ffb9396e52df8191
Camille Dupont <camille.dupont@cea.fr>
2020-11-30 11:30:56 +0100

    Add version update script

---
+++ changeversion.js
@@ -0,0 +1,35 @@
+const glob = require( 'glob' );
+const fs = require('fs');
+
+// new version number
+const VERSION = "0.5.1";
+
+// util function to update version in given package.json
+function updatePackage(filename) {
+    const p = JSON.parse(fs.readFileSync(filename));
+    p.version = VERSION;
+    Object.keys(p.dependencies).forEach((d) => {
+        if (d.includes("@pixano")) {
+            p.dependencies[d] = VERSION;
+        }
+    });
+    const output = JSON.stringify(p, null, 2);
+    fs.writeFileSync(filename, output);
+}
+
+// update version in demos
+glob( 'demos/*/package.json', ( err, files ) => {
+    if (files) {
+        files.forEach(updatePackage);
+    }
+});
+
+// update version in packages
+glob( 'packages/*/package.json', ( err, files ) => {
+    if (files) {
+        files.forEach(updatePackage);
+    }
+});
+
+// update root version
+updatePackage('package.json');
